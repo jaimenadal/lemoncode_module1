ec776f878dd88d6df5545681153742aade072b61
Jaime <jaimenadallopez@gmail.com>
2025-04-22 13:50:51 +0200

    Create script1.sh

---
+++ script1.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+param="$1"
+
+if [ -d foo/ ]; then
+
+echo "El directorio foo ya está creado"
+
+else
+mkdir foo/
+mkdir -p foo/dummy foo/empty
+cd foo/dummy
+echo 'Me encanta la bash!!' > file1.txt
+touch file2.txt
+cat file1.txt > file2.txt
+mv file2.txt ../empty
+
+fi
+
+
+
+ if [ -z "$param" ]; then
+
+          param="Que me gusta la bash!!!!"
+          echo $param > file1.txt
+          cat file1.txt
+ else
+
+         echo $param > file1.txt
+         cat file1.txt
+ fi
